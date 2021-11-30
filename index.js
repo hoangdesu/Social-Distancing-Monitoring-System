0939f0ef75268cf17de80cf733b1fb65fae56ae5
hoangdesu <hoangdesu@gmail.com>
2021-11-30 14:55:51 +0700

    requestDemo

---
+++ index.js
@@ -0,0 +1,7 @@
+// fetch("http://localhost:3001/products")
+//     .then(res => res.json())
+//     .then(data => {
+//         document.querySelector('h1').textContent = data.brand;
+//         document.querySelector('h2').textContent = data.price;
+//     })
+
