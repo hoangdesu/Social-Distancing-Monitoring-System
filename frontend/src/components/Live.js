dc30aeb843673e486f5d08174a523295393d0c10
hoangdesu <hoangdesu@gmail.com>
2021-12-07 18:33:33 +0700

    Added CORS on Flask

--- frontend/src/components/Live.js
+++ frontend/src/components/Live.js
@@ -1,22 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
+import axios from 'axios';
 
 const Live = () => {
-    const videoConstraints = {
-        width: 1280,
-        height: 720,
-        facingMode: 'user',
-    };
-    const webcamRef = React.useRef(null);
+    // const videoConstraints = {
+    //     width: 1280,
+    //     height: 720,
+    //     facingMode: 'user',
+    // };
+    // const webcamRef = React.useRef(null);
 
-    const capture = React.useCallback(() => {
-        const imageSrc = webcamRef.current.getScreenshot();
-    }, [webcamRef]);
+    // const capture = React.useCallback(() => {
+    //     const imageSrc = webcamRef.current.getScreenshot();
+    // }, [webcamRef]);
+
+
+
+    // --- video feed states
+    const [videoSrc, setVideoSrc] = useState(<img src="http://192.168.1.6:8000/video_feed" alt=""></img>);
+
+    
+    // using Flask server
+    const VIDEO_FEED_URL = 'http://192.168.1.6:8000/video_feed'
+
+    useEffect(() => {
+        console.log("LIVE!!");
+        // axios.get(VIDEO_FEED_URL)
+        //     .then(data => {
+        //         console.log("Video:", data);
+        //     })
+        //     .catch(e => {
+        //         console.log("Error:", e);
+        //     })
+        fetch(VIDEO_FEED_URL)
+            .then(res => {
+                if (res.ok) {
+                    console.log("Fetched video OK");
+                } else {
+                    console.log("fetched error")
+                }
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }, []);
+
+    const refetchVideo = () => {
+        fetch(VIDEO_FEED_URL)
+            .then(res => {
+                if (res.ok) {
+                    console.log("Fetched video OK");
+                    setVideoSrc(<img src="http://192.168.1.6:8000/video_feed" alt=""></img>)
+                } else {
+                    console.log("fetched error")
+                }
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
 
     return (
         <div>
-            CAM1
-            <img src="http://192.168.1.6:8000/video_feed" alt="" />
+            <h1>QR Reader</h1>
+            {/* <img src="http://192.168.1.6:8000/video_feed" alt="" /> */}
+            {videoSrc}
+            <button onClick={refetchVideo}>Refetch video</button>
             {/* CAM2
             <Webcam
                 audio={false}
