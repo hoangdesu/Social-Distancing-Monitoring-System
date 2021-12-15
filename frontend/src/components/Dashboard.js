2cb769ba4e5c287af4b3da8a43899377972f38b8
hoangdesu <hoangdesu@gmail.com>
2021-12-15 20:25:03 +0700

    Update dashboard UI with placeholder components

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,196 +1,220 @@
 import React, { useState, useEffect } from 'react';
 import faker from 'faker';
-import temp_icon from './../assets/temp-icon.jpg'
-import moist_icon from './../assets/moist-icon.jpg'
-import humid_icon from './../assets/humid-icon.jpg'
-import human_icon from './../assets/people-icon.jpg'
-//import textfile from "../../../backend/habitat/message.txt"
-import {
-    Chart as ChartJS,
-    ArcElement,
-    Tooltip,
-    Legend,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-} from 'chart.js';
-import { Chart, Doughnut, Line } from 'react-chartjs-2';
+import temp_icon from './../assets/temp-icon.jpg';
+import moist_icon from './../assets/moist-icon.jpg';
+import humid_icon from './../assets/humid-icon.jpg';
+import human_icon from './../assets/people-icon.jpg';
+import { Humidity } from 'react-environment-chart';
 import DashboardCSS from './Dashboard.module.css';
 import axios from 'axios';
-import DashElementSmall from './DashElementSmall';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./fontawasome.js"
+import { faUser, faStreetView } from '@fortawesome/free-solid-svg-icons';
 
-{
-  /* <Chart
-  type={...}
-  options={...}
-  data={...}
-  {...props}
-/> */
-}
+import Thermometer from './Thermometer';
 
+// API ENDPOINTS
+const LATEST_MEASUREMENTS = 'http://localhost:7000/measurements/latest/';
+const VIDEO_FEED = 'http://192.168.137.51:5000/video_feed';
+const ALL_ENTRY = 'http://localhost:7000/entry/all';
+const LATEST_MESSAGE = 'http://localhost:7000/message/latest';
+const UPDATE_INTERVAL = 1000 * 5; // ms
 
-// SAMPLE DATA
-ChartJS.register(
-    ArcElement,
-    Tooltip,
-    Legend,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
+// class MyDashBoard extends React.Component {
+//     componentDidMount() {
+//         this.getData();
+//         this.interval = setInterval(() => {
+//             this.getData();
+//         }, 3000);
+//     }
 
-export const data = {
-    // labels: ['Red', 'Blue'],
-    labels: [],
-    datasets: [
-        {
-            // label: ' # of Votes',
-            data: [10, 1],
-            backgroundColor: [
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
-            borderWidth: 1,
-        },
-    ],
-};
+//     getData() {
+// axios.get('http://localhost:7000/measurements/latest/').then((res) => {
+//     if (parseFloat(res.data[0].celcius) !== 0) {
+//         console.log(res.data[0]);
+//         document.getElementById('temp-label').innerHTML =
+//             'Temperature: ' + res.data[0].celcius + ' °C';
+//         document.getElementById('humid-label').innerHTML =
+//             'Humidity: ' + res.data[0].humidity + ' %';
+//         document.getElementById('moist-label').innerHTML =
+//             'Moisture: ' + res.data[0].moisture + ' %';
+//         if (res.data[0].created != undefined) {
+//             const time = res.data[0].created.split('T');
+//             const rm_mili = time[1].split('.');
+//             document.getElementById('update-time').innerHTML =
+//                 'Last Updated: ' +
+//                 time[0] +
+//                 ' at ' +
+//                 rm_mili[0] +
+//                 ' GMT: 0';
+//         }
+//     }
+// });
 
-// SAMPLE DATA
+// axios.get('http://localhost:7000/entry/all').then((res) => {
+//     console.log(res.data[0]);
 
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
-        title: {
-            display: true,
-            text: 'Total number of people going in and out',
-        },
-    },
-};
+//     if (parseInt(res.data[0].entry_number) < 0)
+//         res.data[0].entry_number = '0';
+//     document.getElementById('people-label').innerHTML =
+//         'Number of people present: ' + res.data[0].entry_number;
+// });
 
-const labels = ['In', 'Out'];
+// axios.get('http://localhost:7000/message/latest').then((res) => {
+//     console.log(res.data[0]);
+//     if (res.data[0].content == 'QR Scan is done') {
+//         document.getElementById('qr-mask').style.display = 'none';
+//     } else if ((res.data[0].content = 'QR Check!')) {
+//         document.getElementById('qr-mask').style.display = 'block';
+//     }
+// });
+// }
 
-export const data1 = {
-    labels,
-    datasets: [
-        {
-            label: 'In',
-            data: labels.map(() =>
-                faker.datatype.number({ min: -1000, max: 1000 })
-            ),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: 'Out',
-            data: labels.map(() =>
-                faker.datatype.number({ min: -1000, max: 1000 })
-            ),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        },
-    ],
-};
+// componentWillUnmount() {
+//     clearInterval(this.interval);
+// }
 
-class FancyButton extends React.Component {
-    // This syntax ensures `this` is bound within handleClick.  // Warning: this is *experimental* syntax.  
-    handleClick = () => {
-        if (document.getElementById("qr-mask").style.display ===  "none")    
-            document.getElementById("qr-mask").style.display = "block"
-        else
-            document.getElementById("qr-mask").style.display = "none"
-    }
-    render() {
-      return (
-        <button onClick={this.handleClick}>
-          Click me
-        </button>
-      );
-    }
-  }
-
-class MyDashBoard extends React.Component {
-    measurements = {
-        moisture: "",
-        temperature: "",
-        humidity: ""
-      };
-
-    componentDidMount() {
-      this.getData();
-      this.interval = setInterval(() => {
-        this.getData();
-      }, 3000);
-    }
-
-  
-    getData() {
-        axios.get('http://localhost:7000/measurements/latest/')
-        .then(res => {
-            if (parseFloat(res.data[0].celcius) !== 0) {
-                console.log(res.data[0])
-                document.getElementById("temp-label").innerHTML = 'Temperature: ' + res.data[0].celcius + ' °C'
-                document.getElementById("humid-label").innerHTML = 'Humidity: ' + res.data[0].humidity + ' %'
-                document.getElementById("moist-label").innerHTML = 'Moisture: ' + res.data[0].moisture + ' %'
-                if (res.data[0].created != undefined) {
-                    const time = res.data[0].created.split("T")
-                    const rm_mili = time[1].split(".")
-                    document.getElementById("update-time").innerHTML = 'Last Updated: ' + time[0] + ' at ' + rm_mili[0] + ' GMT: 0'
-                }
-            }
-        })
-
-        axios.get('http://localhost:7000/entry/all')
-        .then(res => {
-            console.log(res.data[0]);
- 
-            if (parseInt(res.data[0].entry_number) < 0) res.data[0].entry_number = "0";
-            document.getElementById("people-label").innerHTML = 'Number of people present: ' + res.data[0].entry_number;
-        })
-
-        axios.get('http://localhost:7000/message/latest')
-        .then(res => {
-            console.log(res.data[0])
-            if (res.data[0].content == "QR Scan is done") {
-                document.getElementById("qr-mask").style.display = "none"
-            }
-            
-            else if (res.data[0].content = "QR Check!") {
-                document.getElementById("qr-mask").style.display = "block"
-            }
-        })
-    }
-  
-   componentWillUnmount() {
-     clearInterval(this.interval);
-   }
-  
-    render() {
-      return <h3 id="update-time">Last Updated: </h3>;
-    }
-  }
+// render() {
+//     return <h3 id="update-time">Last Updated: </h3>;
+// }
+// }
 
 const Dashboard = () => {
-    const URL_video = "http://192.168.137.51:5000/video_feed"
+    // --- STATES ---
+    const [measurements, setMeasurements] = useState({
+        temperature: 27,
+        moisture: 1,
+        humidity: 2,
+        created: 'Created Time (placeholder)',
+    });
+    const [peopleNum, setPeopleNum] = useState(0);
+    const [serverConnected, setServerConnected] = useState(false);
+    const [camIsOn, setCamIsOn] = useState(false);
+
+    // --- FUNCTIONS ---
+    const fetchMeasurements = () => {
+        axios
+            .get(LATEST_MEASUREMENTS)
+            .then((res) => {
+                let { celcius, moisture, humidity } = res.data[0].celcius;
+
+                // condition checks before setting measurements
+                if (parseFloat(celcius) !== 0) {
+                    setMeasurements((prev) => ({
+                        ...prev,
+                        temperature: celcius,
+                    }));
+                }
+                if (parseFloat(moisture) !== 0) {
+                    setMeasurements((prev) => ({
+                        ...prev,
+                        moisture: moisture,
+                    }));
+                }
+                if (parseFloat(humidity) !== 0) {
+                    setMeasurements((prev) => ({
+                        ...prev,
+                        humidity: humidity,
+                    }));
+                }
+            })
+            .catch((e) => {
+                setServerConnected(false);
+                setMeasurements((prev) => ({
+                    ...prev,
+                    temperature: Math.floor(Math.random() * 50),
+                    humidity: Math.floor(Math.random() * 100),
+                })); // for testing only
+                console.log('Error fetching measurements -', e);
+            });
+    };
+
+    const fetchPeopleNumberInRoom = () => {
+        axios
+            .get(ALL_ENTRY)
+            .then((res) => {
+                let { entry_number } = parseInt(res.data[0]);
+                if (parseInt(entry_number) < 0) entry_number = 0;
+                setPeopleNum(entry_number);
+            })
+            .catch((e) => {
+                setServerConnected(false);
+                console.log('Error fetching number of people in room -', e);
+            });
+    };
+
+    const fetchLatestMessage = () => {
+        axios
+            .get(LATEST_MESSAGE)
+            .then((res) => {
+                let { content } = res.data[0];
+                if (content === 'QR Scan is done') {
+                    // update QR scan camera
+                } else if (content === 'QR Check!') {
+                    // update QR scan camera
+                }
+            })
+            .catch((e) => {
+                console.log('Error fetching latest message -', e);
+            });
+    };
+
+    // --- EFFECTS ---
+    useEffect(() => {
+        setInterval(() => {
+            // fetchMeasurements();
+            // fetchPeopleNumberInRoom();
+            // fetchLatestMessage();
+        }, UPDATE_INTERVAL);
+    }, []);
 
     return (
-       
         <div className={DashboardCSS.container}>
-            <MyDashBoard/>
-            {/* <p>Dashboard</p> */}
-            {/* <DashElementSmall styling={DashboardCSS['small-element']} />
-            <DashElementSmall styling={DashboardCSS['small-element']} />
-            <DashElementSmall styling={DashboardCSS['small-element']} />
-            <DashElementSmall styling={DashboardCSS['small-element']} /> */}
+            {/* <MyDashBoard /> */}
+            <div
+                className={`${DashboardCSS['card']} ${DashboardCSS['top-pane']}`}
+            >
+                {/* <div>
+                    <h2>Measurements</h2>
+                    
+                    <p>Service status: (connected)</p>
+                    <p>QR scanner: on</p>
+                    <p>(system info)</p>
+                </div> */}
+                <div>
+                    <Thermometer
+                        theme="light"
+                        value={measurements.temperature}
+                        max={50}
+                        steps="1"
+                        format="°C"
+                        size="small"
+                        height="150"
+                    />
+                    <h4>Temperature</h4>    
+                </div>
+                <div>
+                <Humidity
+                    value={measurements.humidity}
+                    height={100}
+                    tips={['10%', '60%', 'three']}
+                />
+                <h4>Humidity</h4>
+                </div>
+                <div>
+                    <h3>Moisture chart</h3>
+                    <p>?</p>
+                </div>
+                <div>
+                <FontAwesomeIcon icon={faStreetView} color='green' size="2x" fixedWidth swapOpacity spin={false} mask={['far', 'circle']}/>
+                <FontAwesomeIcon icon={faStreetView} color='green' size="2x" fixedWidth swapOpacity spin={false} mask={['far', 'circle']}/>
+                <FontAwesomeIcon icon={faStreetView} color='green' size="2x" fixedWidth swapOpacity spin={false} mask={['far', 'circle']}/>
+                <FontAwesomeIcon icon={faStreetView} color='pink' size="2x" fixedWidth swapOpacity spin={false} mask={['far', 'circle']}/>
+                <FontAwesomeIcon icon={faStreetView} color='pink' size="2x" fixedWidth swapOpacity spin={false} mask={['far', 'circle']}/>
+                <h3>People in room: 3/5</h3>
+                </div>
+                
+            </div>
 
             <div className={DashboardCSS['first-row']}>
                 <div
@@ -223,6 +247,7 @@ const Dashboard = () => {
                     className={`${DashboardCSS['card']} ${DashboardCSS['small-element']}`}
                 >
                     <h3 id="moist-label">Moisture: 50%</h3>
+                    
                     <img
                         src={moist_icon}
                         width={'70%'}
@@ -234,7 +259,7 @@ const Dashboard = () => {
             </div>
 
             <div className={`${DashboardCSS['second-row']}`}>
-                <div 
+                <div
                     className={`${DashboardCSS['card']} ${DashboardCSS['small-element']}`}
                 >
                     <h3 id="people-label">Number of people present: 3</h3>
@@ -246,28 +271,18 @@ const Dashboard = () => {
                         options={{ maintainAspectRatio: false }}
                     />
                 </div>
-                <div className={`${DashboardCSS['card']} ${DashboardCSS['medium-element']}`}>
-                    <h3>Members - Group 4</h3>
-                    <p>s3755614,Tran Kim Long,0797999956</p>
-                    <p>s3694551,Bui Minh Triet,0903162810</p>
-                    <p>s3818298,Tran Minh Hoang,0819342323</p>
-                    <p>s3688165,Tran Hoang Nam,0981491095</p>
-                    <p>s3772163,Ta Hien Thuan,0703306422</p>
-                    <p>s3697305,Nguyen Quoc Hoang,0913172602</p>
-                    <p>s3740941,Lee Eunseo,0368773931</p>
-                </div>
-                <div 
-                    id = "qr-mask" className={`${DashboardCSS['card']} ${DashboardCSS['medium-element']}`}
+
+                <div
+                    id="qr-mask"
+                    className={`${DashboardCSS['card']} ${DashboardCSS['medium-element']}`}
                 >
                     <div>
-                        <img src={URL_video} alt="QR_Camera" />
+                        <img src={VIDEO_FEED} alt="QR_Camera" />
                     </div>
                     {/* <h3>Total people</h3>
                     <Line options={options} data={data1} /> */}
                 </div>
             </div>
-
-            {/* <img src="https://cdn.mos.cms.futurecdn.net/S5bicwPe8vbP9nt3iwAwwi.jpg" style={{ width: '80%'}}></img> */}
         </div>
     );
 };
